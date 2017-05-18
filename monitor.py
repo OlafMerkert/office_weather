a4b7e8933a07d9703e789f9761a41db19d9dd2e3
Olaf Merkert <olaf.merkert@tngtech.com>
2017-05-18 13:02:48 +0200

    disable librato functionality

--- monitor.py
+++ monitor.py
@@ -3,19 +3,17 @@
 # based on code by henryk ploetz
 # https://hackaday.io/project/5301-reverse-engineering-a-low-cost-usb-co-monitor/log/17909-all-your-base-are-belong-to-us
 
-import os, sys, fcntl, time, librato, yaml, socket
-
+import fcntl
+import os
 import requests
+import socket
+import sys
+import time
+import yaml
 
-def callback_function(error, result):
-    if error:
-        print(error)
-        return
 
-    print(result)
-
-def decrypt(key,  data):
-    cstate = [0x48,  0x74,  0x65,  0x6D,  0x70,  0x39,  0x39,  0x65]
+def decrypt(key, data):
+    cstate = [0x48, 0x74, 0x65,  0x6D,  0x70,  0x39,  0x39,  0x65]
     shuffle = [2, 4, 0, 7, 1, 6, 5, 3]
 
     phase1 = [0] * 8
@@ -28,11 +26,11 @@ def decrypt(key,  data):
 
     phase3 = [0] * 8
     for i in range(8):
-        phase3[i] = ( (phase2[i] >> 3) | (phase2[ (i-1+8)%8 ] << 5) ) & 0xff
+        phase3[i] = ((phase2[i] >> 3) | (phase2[(i - 1 + 8) % 8] << 5)) & 0xff
 
     ctmp = [0] * 8
     for i in range(8):
-        ctmp[i] = ( (cstate[i] >> 4) | (cstate[i]<<4) ) & 0xff
+        ctmp[i] = ((cstate[i] >> 4) | (cstate[i] << 4)) & 0xff
 
     out = [0] * 8
     for i in range(8):
@@ -40,11 +38,14 @@ def decrypt(key,  data):
 
     return out
 
+
 def hd(d):
     return " ".join("%02X" % e for e in d)
 
+
 def now():
     return int(time.time())
+
 
 def notifySlack(co2, config, upper_threshold):
     if ((not config) or (not "webhook" in config)):
@@ -69,12 +70,6 @@ def notifySlack(co2, config, upper_threshold):
     except:
         print "Unexpected error:", sys.exc_info()[0]
 
-def publish(client, prefix, co2, tmp):
-    try:
-        client.submit(prefix + ".co2", co2)
-        client.submit(prefix + ".tmp", tmp)
-    except:
-        print "Unexpected error:", sys.exc_info()[0]
 
 def config(config_file=None):
     """Get config from file; if no config_file is passed in as argument
@@ -87,9 +82,6 @@ def config(config_file=None):
     with open(config_file, 'r') as stream:
         return yaml.load(stream)
 
-def client(config):
-    return librato.connect(config["user"], config["token"])
-
 
 if __name__ == "__main__":
     """main"""
@@ -97,17 +89,17 @@ if __name__ == "__main__":
     # use lock on socket to indicate that script is already running
     try:
         s = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
-        ## Create an abstract socket, by prefixing it with null.
+        # Create an abstract socket, by prefixing it with null.
         s.bind('\0postconnect_gateway_notify_lock')
     except socket.error, e:
         # if script is already running just exit silently
         sys.exit(0)
 
     key = [0xc4, 0xc6, 0xc0, 0x92, 0x40, 0x23, 0xdc, 0x96]
-    fp = open(sys.argv[1], "a+b",  0)
+    co2_monitor_device_handle = open(sys.argv[1], "a+b",  0)
     HIDIOCSFEATURE_9 = 0xC0094806
     set_report = "\x00" + "".join(chr(e) for e in key)
-    fcntl.ioctl(fp, HIDIOCSFEATURE_9, set_report)
+    fcntl.ioctl(co2_monitor_device_handle, HIDIOCSFEATURE_9, set_report)
 
     values = {}
     stamp = now()
@@ -118,10 +110,10 @@ if __name__ == "__main__":
     except IndexError:
         config = config()
 
-    client = client(config)
+    # client = client(config)
 
     while True:
-        data = list(ord(e) for e in fp.read(8))
+        data = list(ord(e) for e in co2_monitor_device_handle.read(8))
         decrypted = decrypt(key, data)
         if decrypted[4] != 0x0d or (sum(decrypted[:3]) & 0xff) != decrypted[3]:
             print hd(data), " => ", hd(decrypted),  "Checksum error"
@@ -132,7 +124,7 @@ if __name__ == "__main__":
 
             if (0x50 in values) and (0x42 in values):
                 co2 = values[0x50]
-                tmp = (values[0x42]/16.0-273.15)
+                tmp = (values[0x42] / 16.0 - 273.15)
 
                 # check if it's a sensible value
                 # (i.e. within the measuring range plus some margin)
@@ -142,18 +134,21 @@ if __name__ == "__main__":
                 print "CO2: %4i TMP: %3.1f" % (co2, tmp)
                 if now() - stamp > 5:
                     print ">>>"
-                    publish(client, config["prefix"], co2, tmp)
+                    # publish(client, config["prefix"], co2, tmp)
 
                     # publish to slack, if configured
                     if ("slack" in config):
-                        upper_threshold = config["slack"]["upper_threshold"] if "upper_threshold" in config["slack"] else 800
-                        lower_threshold = config["slack"]["lower_threshold"] if "lower_threshold" in config["slack"] else 600
+                        upper_threshold = config["slack"][
+                            "upper_threshold"] if "upper_threshold" in config["slack"] else 800
+                        lower_threshold = config["slack"][
+                            "lower_threshold"] if "lower_threshold" in config["slack"] else 600
                         if (co2 > upper_threshold) and (not notified):
                             notified = True
                             notifySlack(co2, config["slack"], upper_threshold)
                         elif (co2 < lower_threshold):
                             if (notified):
-                                notifySlack(co2, config["slack"], upper_threshold)
+                                notifySlack(
+                                    co2, config["slack"], upper_threshold)
                             notified = False
 
                     stamp = now()
