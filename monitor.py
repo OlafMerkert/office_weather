e63319b0a3bd8bae02ee06c627d38a8efc377677
Olaf Merkert <olaf.merkert@tngtech.com>
2017-05-19 14:35:59 +0200

    refactor to using classes and escalate messages stepwise

--- monitor.py
+++ monitor.py
@@ -11,161 +11,181 @@ import requests
 import sys
 import time
 import yaml
+from collections import namedtuple
+
+TIME_INTERVAL = 5
 
 
-def decrypt(DEVICE_KEY, data):
-    cstate = [0x48, 0x74, 0x65, 0x6D, 0x70, 0x39, 0x39, 0x65]
-    shuffle = [2, 4, 0, 7, 1, 6, 5, 3]
-
-    phase1 = [0] * 8
-    for i, o in enumerate(shuffle):
-        phase1[o] = data[i]
-
-    phase2 = [0] * 8
-    for i in range(8):
-        phase2[i] = phase1[i] ^ DEVICE_KEY[i]
-
-    phase3 = [0] * 8
-    for i in range(8):
-        phase3[i] = ((phase2[i] >> 3) | (phase2[(i - 1 + 8) % 8] << 5)) & 0xff
-
-    ctmp = [0] * 8
-    for i in range(8):
-        ctmp[i] = ((cstate[i] >> 4) | (cstate[i] << 4)) & 0xff
-
-    out = [0] * 8
-    for i in range(8):
-        out[i] = (0x100 + phase3[i] - ctmp[i]) & 0xff
-
-    return out
+EnvironmentData = namedtuple("EnvironmentData", ["temperature", "co2_level"])
+Boundary = namedtuple("Boundary", ["threshold", "message"])
 
 
-def hd(d):
+def hex_format(d):
     return " ".join("%02X" % e for e in d)
 
 
-def now():
+def current_time():
     return int(time.time())
 
-slack_co2_notified = False
-slack_temperature_notified = False
 
-def notify_slack(temperature, co2_level, config):
-    global slack_co2_notified, slack_temperature_notified
-    upper_co2_threshold = config["upper_co2_threshold"]
-    lower_co2_threshold = config["lower_co2_threshold"]
+class Co2Device(object):
 
-    upper_temperature_threshold = config["upper_temperature_threshold"]
-    lower_temperature_threshold = config["lower_temperature_threshold"]
+    def __init__(self, device_path):
+        self._device_path = device_path
+        self._values = {}
+        self._device_key = [0xc4, 0xc6, 0xc0, 0x92, 0x40, 0x23, 0xdc, 0x96]
 
-    co2_message = None
-    if (co2_level > upper_co2_threshold) and (not slack_co2_notified):
-        slack_co2_notified = True
-        co2_message = "Bitte Fenster oeffnen, der CO2 Level ist bei {0}ppm.".format(co2_level)
-    elif (co2_level < lower_co2_threshold):
-        if (slack_co2_notified):
-            co2_message = "Das Fenster kann geschlossen werden, der CO2 Level ist nur noch bei {0}ppm.".format(co2_level)
-            slack_co2_notified = False
+    def decrypt(self, data):
+        cstate = [0x48, 0x74, 0x65, 0x6D, 0x70, 0x39, 0x39, 0x65]
+        shuffle = [2, 4, 0, 7, 1, 6, 5, 3]
 
-    send_slack_message(config, co2_message)
+        phase1 = [0] * 8
+        for i, o in enumerate(shuffle):
+            phase1[o] = data[i]
 
-    temperature_message = None
-    if (temperature > upper_temperature_threshold) and (not slack_temperature_notified):
-        slack_temperature_notified = True
-        temperature_message = "Heiss hier drinnen, es sind {0} Grad Celsius. Klimaanlage anschalten!".format(temperature)
-    elif (temperature < lower_temperature_threshold):
-        if (slack_temperature_notified):
-            temperature_message = "Es ist wieder kuehler, nur noch {0} Grad Celsius. Die Klimaanlage kann wieder ausgeschaltet werden.'".format(temperature)
-            slack_temperature_notified = False
+        phase2 = [0] * 8
+        for i in range(8):
+            phase2[i] = phase1[i] ^ self._device_key[i]
 
-    # print("debug temp {0}, notification {1}".format(temperature_message, slack_temperature_notified))
-    send_slack_message(config, temperature_message)
+        phase3 = [0] * 8
+        for i in range(8):
+            phase3[i] = ((phase2[i] >> 3) | (phase2[(i - 1 + 8) % 8] << 5)) & 0xff
 
-def send_slack_message(config, message):
-    if (message and config and "webhook" in config):
-        webhook_url = config["webhook"]
-        channel = config["channel"] if "channel" in config else "#general"
-        botName = config["botname"] if "botname" in config else "CO2bot"
-        icon = config["icon"] if "icon" in config else ":robot_face:"
-        # print("debug sending message to slack: {0}".format(message))
-        try:
+        ctmp = [0] * 8
+        for i in range(8):
+            ctmp[i] = ((cstate[i] >> 4) | (cstate[i] << 4)) & 0xff
+
+        out = [0] * 8
+        for i in range(8):
+            out[i] = (0x100 + phase3[i] - ctmp[i]) & 0xff
+
+        return out
+
+    def open_monitor_device(self):
+        self._co2_monitor_device_handle = open(self._device_path, "a+b", 0)
+        HIDIOCSFEATURE_9 = 0xC0094806
+        set_report = "\x00" + "".join(chr(e) for e in self._device_key)
+        fcntl.ioctl(self._co2_monitor_device_handle, HIDIOCSFEATURE_9, set_report)
+
+    def read_device_data(self):
+        data = list(ord(chunk) for chunk in self._co2_monitor_device_handle.read(8))
+        decrypted = self.decrypt(data)
+
+        if decrypted[4] != 0x0d or (sum(decrypted[:3]) & 0xff) != decrypted[3]:
+            print(hex_format(data), " => ", hex_format(decrypted), "Checksum error")
+        else:
+            op = decrypted[0]
+            val = decrypted[1] << 8 | decrypted[2]
+            self._values[op] = val
+
+            if (0x50 in self._values) and (0x42 in self._values):
+                co2_level = self._values[0x50]
+                temperature = (self._values[0x42] / 16.0 - 273.15)
+                if not (co2_level > 5000 or co2_level < 0):
+                    return EnvironmentData(co2_level=co2_level, temperature=temperature)
+
+        return None
+
+
+class SlackReporter(object):
+
+    def __init__(self, config):
+        self._config = config
+        self.load_config(config)
+
+    def load_config(self, config):
+        if "webhook" in config:
+            self._webhook_url = config["webhook"]
+            self._channel = config["channel"]
+            self._botName = config["botname"]
+            self._icon = config["icon"]
+        else:
+            raise "Did not find slack configuration object"
+
+    def enabled_p(self):
+        return (hasattr(self, "_webhook_url") and
+                hasattr(self, "_channel") and
+                hasattr(self, "_botName") and
+                hasattr(self, "_icon"))
+
+    def send_message(self, message):
+        if (message and self.enabled_p()):
+            # print("debug sending message to slack: {0}".format(message))
             payload = {
-                'channel': channel,
-                'username': botName,
+                'channel': self._channel,
+                'username': self._botName,
                 'text': message,
-                'icon_emoji': icon
+                'icon_emoji': self._icon
             }
-            requests.post(webhook_url, json=payload)
-        except:
-            print("Unexpected error:", sys.exc_info()[0])
+            requests.post(self._webhook_url, json=payload)
 
 
-def config(config_file=None):
-    """Get config from file; if no config_file is passed in as argument
-        default to "config.yaml" in script dir"""
+class HystereseNotifier(object):
 
-    if config_file is None:
-        script_base_dir = os.path.dirname(os.path.realpath(sys.argv[0])) + "/"
-        config_file = script_base_dir + "config.yaml"
+    def __init__(self, reporter, data_extractor, lower_boundary, upper_boundary):
+        self._reporter = reporter
+        self._above_upper = False
+        self._lower_boundary = lower_boundary
+        self._upper_boundary = upper_boundary
+        self._data_extractor = data_extractor
 
-    with open(config_file, 'r') as stream:
+    def notify_value(self, value):
+        if not self._above_upper and value > self._upper_boundary.threshold:
+            self._above_upper = True
+            self._reporter.send_message(self._upper_boundary.message.format(value))
+        elif self._above_upper and value <= self._lower_boundary.threshold:
+            self._above_upper = False
+            self._reporter.send_message(self._lower_boundary.message.format(value))
+
+    def notify(self, data):
+        self.notify_value(self._data_extractor(data))
+
+
+def HystereseNotifierFromConfig(reporter, data_extractor, config):
+    return HystereseNotifier(reporter, data_extractor,
+                             lower_boundary=Boundary(threshold=config["lower_threshold"],
+                                                     message=config["lower_message"]),
+                             upper_boundary=Boundary(threshold=config["upper_threshold"],
+                                                     message=config["upper_message"]))
+
+
+def get_config(config_file_path):
+    with open(config_file_path, 'r') as stream:
         return yaml.load(stream)
 
-DEVICE_KEY = [0xc4, 0xc6, 0xc0, 0x92, 0x40, 0x23, 0xdc, 0x96]
 
-def open_monitor_device():
-    co2_monitor_device_handle = open(sys.argv[1], "a+b", 0)
-    HIDIOCSFEATURE_9 = 0xC0094806
-    set_report = "\x00" + "".join(chr(e) for e in DEVICE_KEY)
-    fcntl.ioctl(co2_monitor_device_handle, HIDIOCSFEATURE_9, set_report)
+def main(device_path, config_file_path):
+    config = get_config(config_file_path)
 
-    return co2_monitor_device_handle
+    monitor_device = Co2Device(device_path)
+    monitor_device.open_monitor_device()
 
-def read_device_data(co2_monitor_device_handle):
-    global values
-    data = list(ord(chunk) for chunk in co2_monitor_device_handle.read(8))
-    decrypted = decrypt(DEVICE_KEY, data)
+    observers = []
 
-    if decrypted[4] != 0x0d or (sum(decrypted[:3]) & 0xff) != decrypted[3]:
-        print(hd(data), " => ", hd(decrypted), "Checksum error")
-    else:
-        op = decrypted[0]
-        val = decrypted[1] << 8 | decrypted[2]
-        values[op] = val
+    if "slack" in config:
+        slack = SlackReporter(config["slack"])
+        if "temperature" in config:
+            temperature_observer = HystereseNotifierFromConfig(slack, lambda data: data.temperature, config["temperature"])
+            observers.append(temperature_observer)
+        if "co2_level" in config:
+            co2_observer = HystereseNotifierFromConfig(slack, lambda data: data.co2_level, config["co2_level"])
+            observers.append(co2_observer)
 
-        if (0x50 in values) and (0x42 in values):
-            co2_level = values[0x50]
-            temperature = (values[0x42] / 16.0 - 273.15)
-            return (co2_level, temperature)
-
-    return (None, None)
-
-
-if __name__ == "__main__":
-
-    try:
-        config = config(config_file=sys.argv[2])
-    except IndexError:
-        config = config()
-
-    # client = client(config)
-
-    co2_monitor_device_handle = open_monitor_device()
-    stamp = now()
-    values = {}
+    stamp = current_time()
 
     while True:
-        (co2_level, temperature) = read_device_data(co2_monitor_device_handle)
+        data = monitor_device.read_device_data()
+        print("debug current data: {0}".format(data))
 
-        if co2_level and temperature:
-            # check if it's a sensible value
-            # (i.e. within the measuring range plus some margin)
-            if (co2_level > 5000 or co2_level < 0):
-                continue
+        if data and (current_time() - stamp > TIME_INTERVAL):
+            for o in observers:
+                o.notify(data)
+                stamp = current_time()
 
-            print("CO2: %4i TMP: %3.1f" % (co2_level, temperature))
-            if now() - stamp > 5:
-                print(">>>")
-                if ("slack" in config):
-                    notify_slack(temperature, co2_level, config["slack"])
-                stamp = now()
+if __name__ == "__main__":
+    script_base_dir = os.path.dirname(os.path.realpath(sys.argv[0])) + "/"
+    config_file_path = script_base_dir + "config.yaml"
+
+    device_path = sys.argv[1]
+
+    main(device_path, config_file_path)
