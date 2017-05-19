52e6f2546f4b4f687f19daf6e15d38004f20629c
Olaf Merkert <olaf.merkert@tngtech.com>
2017-05-19 15:23:55 +0200

    initial code for generating plots

--- monitor.py
+++ monitor.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python2
+# -*- coding: utf-8 -*-
 
 from __future__ import print_function
 
@@ -12,12 +13,15 @@ import sys
 import time
 import yaml
 from collections import namedtuple
+from matplotlib import pyplot
+
 
 TIME_INTERVAL = 5
 
 
 EnvironmentData = namedtuple("EnvironmentData", ["temperature", "co2_level"])
 Boundary = namedtuple("Boundary", ["threshold", "message"])
+DataExtractor = namedtuple("DataExtractor", ["label", "extract"])
 
 
 def hex_format(d):
@@ -149,6 +153,28 @@ def HystereseNotifierFromConfig(reporter, data_extractor, config):
                                                      message=config["upper_message"]))
 
 
+class GraphCollector(object):
+
+    def __init__(self, image_reporter, data_extractors=[], data_count=20):
+        self._data_extractors = data_extractors
+        self._data_count = data_count
+        self._data = []
+
+    def notify(self, data):
+        self._data.append(data)
+        if len(self._data) >= self._data_count:
+            self.plot_data()
+
+    def plot_data(self):
+        print("debug creating next plot")
+        x_data = range(len(self._data))
+        for extractor in self._data_extractors:
+            y_data = list(map(extractor.extract, self._data))
+            pyplot.plot(x_data, y_data)
+            pyplot.title(extractor.label)
+            pyplot.show()
+        self._data = []
+
 def get_config(config_file_path):
     with open(config_file_path, 'r') as stream:
         return yaml.load(stream)
@@ -161,6 +187,7 @@ def main(device_path, config_file_path):
     monitor_device.open_monitor_device()
 
     observers = []
+    collectors = []
 
     if "slack" in config:
         slack = SlackReporter(config["slack"])
@@ -174,7 +201,17 @@ def main(device_path, config_file_path):
             observers.append(co2_observer)
             print("debug Enabled watching CO2 level")
 
+    if "plotting" in config:
+        graph_notifier = GraphCollector(None,
+                                        [DataExtractor(label="temperature",
+                                                       extract=lambda data: data.temperature),
+                                         DataExtractor(label="CO2 level",
+                                                       extract=lambda data: data.co2_level)])
+        collectors.append(graph_notifier)
+
     stamp = current_time()
+
+    print("debug observers: {0}, collectors: {1}".format(observers, collectors))
 
     while True:
         data = monitor_device.read_device_data()
@@ -184,6 +221,10 @@ def main(device_path, config_file_path):
             for o in observers:
                 o.notify(data)
                 stamp = current_time()
+        if data:
+            for c in collectors:
+                c.notify(data)
+
 
 if __name__ == "__main__":
     script_base_dir = os.path.dirname(os.path.realpath(sys.argv[0])) + "/"
